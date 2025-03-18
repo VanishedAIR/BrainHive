b8373d775fb73d1a3431603c0fe7689da0d97b30
Jesus-29 <jesussantiago4215@gmail.com>
2025-03-18 15:29:54 -0700

    working dark mode

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,27 +1,29 @@
+import { ModeToggle } from "@/components/ui/toggle";
 import {
-    SignedIn,
-    SignedOut,
-    SignInButton,
-    SignUpButton,
-    UserButton,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
 } from "@clerk/nextjs";
 
 export default function Navbar() {
-    return (
-        <section>
-            <header className="flex justify-between items-center p-4 h-16">
-                {/* logo on the left side of the navbar: */}
-                <img src="/logo.svg" alt="Logo" className="h-20 w-20" />
-                <div className="flex gap-4">
-                    <SignedOut>
-                        <SignInButton mode="modal" />
-                        <SignUpButton />
-                    </SignedOut>
-                    <SignedIn>
-                        <UserButton />
-                    </SignedIn>
-                </div>
-            </header>
-        </section>
-    );
+  return (
+    <section>
+      <header className="flex justify-between items-center p-4 h-16">
+        {/* logo on the left side of the navbar: */}
+        <img src="/logo.svg" alt="Logo" className="h-20 w-20" />
+        <div className="flex gap-4">
+          <SignedOut>
+            <ModeToggle />
+            <SignInButton mode="modal" />
+            <SignUpButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </header>
+    </section>
+  );
 }
