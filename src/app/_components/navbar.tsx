f1c7714281fb499df3b9ff88f5a1f3e21ae3d6dd
VanishedAIR <vanshp.1005@gmail.com>
2025-03-18 22:06:32 -0700

    change hero section and navbar spacing and alignment, new colorway, needs revising

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -13,9 +13,9 @@ export default function Navbar() {
       <header className="flex justify-between items-center p-4 h-25">
         {/* logo on the left side of the navbar: */}
         <img src="/logo.svg" alt="Logo" className="h-20 w-20 mt-1.35" />
-        <div className="flex gap-4">
+        <div className="flex gap-4 flex-row items-center">
           <SignedOut>
-            <ModeToggle />
+            <ModeToggle/>
             <SignInButton mode="modal" />
             <SignUpButton />
           </SignedOut>
