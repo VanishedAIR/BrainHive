84ec6a291fbac6b4b29a25804f3ebf0df70a8618
VanishedAIR <vanshp.1005@gmail.com>
2025-03-19 10:39:25 -0700

    updated colorway for the dark theme, and change the hero section with the plan to update it to hexgonal pattern

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero() {
     return (
-        <div className="fixed flex flex-col items-center justify-center w-[90%] top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 pt-[10vh] pb-[10vh] bg-secondary border-[15px] rounded-4xl border-border">
+        <div className="fixed flex flex-col items-center justify-center w-full h-[75vh] top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
             <h1 className="text-4xl font-bold mb-4">Find Your Hive. Study Smarter, Together.</h1>
             <h2 className="text-lg">Join a buzzing community of learners. Connect with study groups, collaborate, and achieve more—together.</h2>
         </div>
