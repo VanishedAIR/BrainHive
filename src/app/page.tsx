9e26a79633768eabe268ebe54fdb61e8b4105674
Jesus-29 <jesussantiago4215@gmail.com>
2025-03-19 19:40:17 -0700

    synced clerk data with prisma database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-"use client";
 import Hero from "./_components/hero";
 import Navbar from "./_components/navbar";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className={`relative`}>
       <Navbar />
