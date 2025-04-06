c4e837b18694242bf431fa21906f09eaae17a9c6
VanishedAIR <vanshp.1005@gmail.com>
2025-04-06 12:26:11 -0700

    added base formatting for the home page and a filter option, search option. Dropdown menu from ShadCn. Changed the logo to link to the landing page

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,6 +1,7 @@
 import { currentUser } from "@clerk/nextjs/server";
 import { syncUser } from "@/actions/useractions";
 import ClientNavbar from "./ClientNavbar";
+import Link from 'next/link'
 
 export default async function Navbar() {
   const user = await currentUser();
@@ -9,8 +10,10 @@ export default async function Navbar() {
   return (
     <section>
       <header className="flex justify-between items-center p-4 h-25">
+        <Link href = "/">
         <img src="/logo.svg" alt="Logo" className="h-20 w-20 mt-1.35" />
-        <ClientNavbar /> {}
+        </Link>
+        <ClientNavbar />
       </header>
     </section>
   );
