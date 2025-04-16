85d5d37d1904fb670d4e931bad69bf0ee265492e
whosveli <eloisavm22@gmail.com>
2025-04-15 19:20:51 -0700

    testing routing

--- routing test/router.js
+++ routing test/router.js
@@ -2,7 +2,7 @@ document.body.innerHTML = `
     <nav>
         <a href="#home">Home</a>
         <a href="#signin">Sign In</a>
-        <a href="#post">Start Page</a>
+        <a href="#post">Start Page</a> <!-- Add link to the post route -->
     </nav>
     <div id="content"></div>
 `;
@@ -16,13 +16,12 @@ class Router {
 
     handleRouteChange() {
         const hash = window.location.hash.slice(1);
-        console.log(`Current hash: ${hash}`);
+        console.log(hash);
         const route = this.routes[hash];
         if (route) {
-            console.log(`Executing route for hash: ${hash}`);
             document.querySelector('#content').innerHTML = route();
-        } else {
-            console.log(`No route found for hash: ${hash}`);
+        }
+        else {
             document.querySelector('#content').innerHTML = '404 not found';
         }
     }
@@ -31,7 +30,7 @@ class Router {
 const routes = {
     home: () => '<h1>Welcome to the Homepage</h1>',
     signin: () => '<h1>Sign In Page</h1>',
-    post: () => '<h1>Welcome to the Start Page</h1>'
+    post: () => '<h1>Welcome to the Start Page</h1>' // Define the post route here
 };
 
 const router = new Router(routes);
