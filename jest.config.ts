435067593ec2873847ac5dfb743dbd9f5d6bd860
Jesus-29 <jesussantiago4215@gmail.com>
2025-04-27 15:48:36 -0700

    Jest unit testing setup

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig: Config = {
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+};
+
+export default createJestConfig(customJestConfig);
